f5931271c40b147f07aa64e40205cf61a54125c3
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2021-09-05 17:41:57 +0530

    🎉  Day -2

---
+++ MaxSubArraySum.java
@@ -0,0 +1,13 @@
+// Kadene's Algorithm
+
+class Solution {
+    static void maxSum(int arr [], int n){
+        int sum = 0, max=arr[0];
+        for(int i=0; i<n; i++){
+            sum = sum + arr[i];
+            max = Math.max(sum,max);
+            if(sum < 0) sum = 0;
+        }
+        return sum;
+    }
+}
